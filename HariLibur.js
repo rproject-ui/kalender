d448eddb35485216a079d94dd6810aaf5beb446c
rproject-ui <61788897+rproject-ui@users.noreply.github.com>
2026-08-18 15:50:06 +0700

    Create HariLibur.js

---
+++ HariLibur.js
@@ -0,0 +1,18 @@
+var holidayData = [
+  { month: 1, day: 1, name: "Tahun Baru 2026" },
+  { month: 1, day: 15, name: "Isra Mikraj" },
+  { month: 2, day: 17, name: "Tahun Baru Imlek" },
+  { month: 3, day: 19, name: "Hari Suci Nyepi" },
+  { month: 3, day: 20, name: "Idul Fitri 1447H" },
+  { month: 3, day: 21, name: "Idul Fitri 1447H" },
+  { month: 4, day: 3, name: "Wafat Yesus Kristus" },
+  { month: 5, day: 1, name: "Hari Buruh" },
+  { month: 5, day: 14, name: "Kenaikan Yesus Kristus" },
+  { month: 5, day: 31, name: "Hari Raya Waisak" },
+  { month: 6, day: 1, name: "Hari Lahir Pancasila" },
+  { month: 6, day: 27, name: "Idul Adha" },
+  { month: 7, day: 17, name: "Tahun Baru Islam" },
+  { month: 8, day: 17, name: "Hari Kemerdekaan RI" },
+  { month: 9, day: 26, name: "Maulid Nabi" },
+  { month: 12, day: 25, name: "Hari Raya Natal" }
+];
